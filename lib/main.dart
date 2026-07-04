bd92208de9b76737e3c2ea531ef616d0fa8cb357
tangenidonavin4-commits <tangenidonavin4@gmail.com>
2026-07-04 16:42:40 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/simulation_screen.dart';
+
+void main() {
+  runApp(CrashLabApp());
+}
+
+class CrashLabApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CrashLab AI',
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.green,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[900],
+          elevation: 0,
+        ),
+      ),
+      home: SimulationScreen(),
+    );
+  }
+}
